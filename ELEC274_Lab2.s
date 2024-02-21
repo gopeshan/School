992037a17659e173ead04d9f8a1fa0cb63fc07fe
Gopeshan Mathanachandran <122761640+gopeshan@users.noreply.github.com>
2024-02-21 00:01:57 -0500

    Update ELEC274_Lab2.s

--- ELEC274_Lab2.s
+++ ELEC274_Lab2.s
@@ -108,3 +108,85 @@ mov r2, r7
  
 ret
 #--------------------------------------------------------------# end of pt 1
+
+SumList:
+subi sp,sp,4
+ldw r3, 0(sp)
+Loop:
+ldw r4, 0(r2)
+if:
+bgt r5, r0, positive
+beq r5,r0,zerox
+br negative
+ 
+positive:
+subi sp,sp,8
+stw ra,0(sp)
+stw r2,4(sp)
+movi r2,'+'
+call PrintChar
+movi r2,'\n'
+call PrintChar
+ldw ra,0(sp)
+ldw r2,4(sp)
+br endif
+ 
+negative:
+subi sp,sp,8
+stw ra,0(sp)
+stw r2,4(sp)
+movi r2,'-'
+call PrintChar
+movi r2,'\n'
+call PrintChar
+ldw ra,0(sp)
+ldw r2,4(sp)
+br endif
+ 
+zerox:
+subi sp,sp,8
+stw ra,0(sp)
+stw r2,4(sp)
+ 
+movi r2,'0'
+call PrintChar
+movi r2,'\n'
+call PrintChar
+ldw ra,0(sp)
+ldw r2,4(sp)
+br endif
+ 
+ 
+endif:
+subi r3,r3,1
+bgt r3,r0, Loop
+ldw r3, 0(sp)
+addi r2, r2, 4
+ret
+ 
+ 
+ 
+PrintChar:
+subi sp, sp, 8
+stw r3, 4(sp)
+stw r4, 0(sp)
+movia r3, JTAG_UART_BASE
+pc_loop:
+ldwio r4, STATUS_OFFSET(r3)
+andhi r4, r4, WSPACE_MASK
+beq r4, r0, pc_loop
+stwio r2, DATA_OFFSET(r3)
+ldw r3, 4(sp)
+ldw r4, 0(sp)
+addi sp, sp, 8
+ret
+
+_end:
+ 
+.org 0x1000
+ 
+LIST1:  .word 2,4,6
+ 
+LIST2:  .word -1,-1,-1
+ 
+.end
