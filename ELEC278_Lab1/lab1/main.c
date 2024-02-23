dbd216eb6c4511f2282a83cc3e19efd0808c1531
Gopeshan Mathanachandran <122761640+gopeshan@users.noreply.github.com>
2024-02-23 00:35:15 -0500

    Create ELEC278_Lab1

---
+++ ELEC278_Lab1/lab1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int array_average(const int *array, size_t count) {
+    // TODO: return the average of the 'count' elements of 'array'.
+    return 0;
+}
+
+int main() {
+    // TODO: construct an array, call 'array_average' on it, and print the result.
+    printf("Hello, World!\n");
+    return 0;
+}
