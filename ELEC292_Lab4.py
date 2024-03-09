3d458e4e77e7b5f403786791f4fd582cc023eb81
Gopeshan Mathanachandran <122761640+gopeshan@users.noreply.github.com>
2024-03-09 17:15:03 -0500

    Update ELEC292_Lab4.py

--- ELEC292_Lab4.py
+++ ELEC292_Lab4.py
@@ -1,1 +1,26 @@
+import matplotlib.pyplot as plt
+import pandas as pd
 
+#Question 1
+dataset = pd.read_csv('heart.csv')
+data = dataset.iloc[:, : -1]
+labels = dataset.iloc[:, -1]
+
+fig, ax = plt.subplots(ncols=4, nrows=4, figsize=(20,10))
+data.hist(ax=ax.flatten()[:13])
+
+fig.tight_layout()
+plt.show()
+
+#Question 3
+dataset = pd.read_csv('heart.csv')
+data = dataset.iloc[:, : -1]
+labels = dataset.iloc[:, -1]
+
+fig, ax = plt.subplots(ncols=4, nrows=4, figsize=(20,10))
+for i in range(0, 12):
+    ax.flatten()[i].hist(data.iloc[:, i])
+    ax.flatten()[i].set_title(data.columns[i], fontsize=15)
+
+fig.tight_layout()
+plt.show()
