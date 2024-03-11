e6ed32eb5c972cacf42724177a5af28fbbbd0b0a
Gopeshan Mathanachandran <122761640+gopeshan@users.noreply.github.com>
2024-03-10 23:31:25 -0400

    Update ELEC292_Lab4.py

--- ELEC292_Lab4.py
+++ ELEC292_Lab4.py
@@ -24,3 +24,28 @@ for i in range(0, 12):
 
 fig.tight_layout()
 plt.show()
+
+#Question 4
+dataset = pd.read_csv('heart.csv')
+data = dataset.iloc[:, : -1]
+labels = dataset.iloc[:, -1]
+
+fig, ax = plt.subplots(ncols=4, nrows=4, figsize=(20,10))
+
+data.plot(ax=ax.flatten()[:13], kind='density', subplots=True, sharex=False)
+
+fig.tight_layout()
+plt.show()
+
+#Question 5
+dataset = pd.read_csv('heart.csv')
+data = dataset.iloc[:, : -1]
+labels = dataset.iloc[:, -1]
+
+fig, ax = plt.subplots(ncols=4, nrows=4, figsize=(20,10))
+
+data.plot(ax=ax.flatten()[:13], kind='box', subplots=True, sharex=False, sharey=False)
+
+fig.tight_layout()
+plt.show()
+
