2451c37a476daee9109b7c7dc3e1afb1c23e4be2
Gopeshan Mathanachandran <122761640+gopeshan@users.noreply.github.com>
2024-03-15 17:21:53 -0400

    Update ELEC292_Lab4.py

--- ELEC292_Lab4.py
+++ ELEC292_Lab4.py
@@ -125,3 +125,39 @@ ax.set_title('PCA of Wine Quality Dataset', fontsize=15)
 ax.legend(labels, fontsize=15)
 
 plt.show()
+
+# question 9
+
+wine_data = pd.read_csv("winequalityN.csv")
+wine_data['quality'] = (wine_data['quality'] >= 8).astype(int)
+features = wine_data.iloc[:, 1:]
+target = wine_data['quality']
+
+scaler = StandardScaler()
+standardized_features = scaler.fit_transform(features)
+
+pca = PCA(n_components=11)
+pca_components = pca.fit_transform(standardized_features)
+
+fig, ax = plt.subplots(figsize=(10, 10))
+colors = ['pink', 'red']
+labels = ['low-quality', 'quality']
+
+pca_8 = pca_components[:, 8]
+pca_9 = pca_components[:, 9]
+
+for label_value, color in zip(set(target), colors):
+    ax.scatter(
+        pca_8[target == label_value],
+        pca_9[target == label_value],
+        c=color,
+        s=60,
+        label=labels[label_value]
+    )
+
+ax.set_title('PCA of Wine Quality Dataset', fontsize=15)
+ax.set_xlabel('Principal Component - 8', fontsize=15)
+ax.set_ylabel('Principal Component - 9', fontsize=15)
+ax.legend(labels, fontsize=15)
+
+plt.show()
